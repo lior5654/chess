6b3bb50113888ded6ecc0f4a4365ec190e918a2e
Jacob Galam <hgecdkto@gmail.com>
2019-12-24 18:20:42 +0200

    finish Position class

--- Pawn.h
+++ Pawn.h
@@ -4,6 +4,7 @@ class Pawn :
 	public Solider
 {
 public:
+	
 	Pawn(Position position, Color color);
 	bool virtual canMove(Position& dest) override;
 	char virtual GetRepresentation() override;
--- Position.h
+++ Position.h
@@ -1,10 +1,16 @@
 #pragma once
+
+#define BOARD_SIZE 8
+
 class Position
 {
 public:
 	Position(unsigned int col, unsigned int raw);
+	Position(char col, char raw);
 
 	unsigned int col;
 	unsigned int raw;
+private:
+	static bool badIndex(unsigned int index);  // if is less than 0 or more than BOARD_SIZE
 };
 
