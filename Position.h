725c8e717b9bb68a29767f463885a34030d489df
Jacob Galam <hgecdkto@gmail.com>
2019-12-24 18:32:04 +0200

    finish pos class

--- Position.h
+++ Position.h
@@ -6,16 +6,13 @@ class Position
 {
 public:
 	Position(unsigned int col, unsigned int raw);
-<<<<<<< HEAD
 	Position(char col, char raw);
 
 	unsigned int col;
 	unsigned int raw;
 private:
 	static bool badIndex(unsigned int index);  // if is less than 0 or more than BOARD_SIZE
-=======
 	unsigned int col;
 	unsigned int row;
->>>>>>> 5f2baba73d70dca0334398af8846c14b505480b6
 };
 
