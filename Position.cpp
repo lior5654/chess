6424523299576f89162a3bf1926031f84b11342d
BuildTools <unconfigured@null.spigotmc.org>
2019-12-25 14:46:15 +0200

    Added A new ctor for the position class, an == operator, an implicit conversion poerator and setters

--- Position.cpp
+++ Position.cpp
@@ -21,6 +21,10 @@ Position::Position(char col, char row)
 	*this = Position(((unsigned int)'a' - col), (row - (unsigned int)'0' - 1));
 }
 
+Position::Position(unsigned int index)
+{
+	*this = Position(index % BOARD_SIZE, index / BOARD_SIZE);
+}
 Position& Position::operator=(const Position& other)
 {
 	*this = Position(other.column(), other.row());
@@ -39,7 +43,33 @@ unsigned int Position::row() const
 	return this->_row;
 }
 
+void Position::setColumn(unsigned int newCol)
+{
+	if (badIndex(newCol))
+	{
+		throw "Position Out of Range";
+	}
+	this->_col = newCol;
+}
+void Position::setRow(unsigned int newRow)
+{
+	if (badIndex(newRow))
+	{
+		throw "Position Out of Range";
+	}
+	this->_row = newRow;
+}
+
 bool Position::badIndex(unsigned int index)
 {
 	return (index >= BOARD_SIZE);
+}
+
+bool Position::operator==(const Position& other) const
+{
+	return (this->row() == other.row() && this->column == other.column());
+}
+Position::operator unsigned int() const
+{
+	return this->row() * BOARD_SIZE + this->column();
 }
--- Position.h
+++ Position.h
@@ -8,12 +8,16 @@ public:
 	Position();
 	Position(unsigned int col, unsigned int row);
 	Position(char col, char row);
+	Position(unsigned int index);
 	Position& operator=(const Position& other);
 	unsigned int column() const;
 	unsigned int row() const;
+	void setColumn(unsigned int newCol);
+	void setRow(unsigned int newRow);
+	operator unsigned int() const;
+	bool operator==(const Position& other) const;
 private:
 	static bool badIndex(unsigned int index);  // NOTE: unsigned int is by definition bigger than 0, only testing the upper limit.
 	unsigned int _col;
 	unsigned int _row;
 };
-
