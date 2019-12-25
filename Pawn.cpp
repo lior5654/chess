172aeb17acb80ca45b7becc8d71be62d25683a58
Jacob Galam <hgecdkto@gmail.com>
2019-12-25 20:22:14 +0200

    start Pawn class

---
+++ Pawn.cpp
@@ -0,0 +1,31 @@
+#include "Pawn.h"
+
+Pawn::Pawn(Position position, Color color) : Solider(position, color)
+{
+
+}
+
+bool Pawn::canMove(const Position& dest, const Board& gameBoard) const
+{
+	// check if move one
+	Position myPos = this->position();
+	Position tempPos = this->position();
+
+	unsigned int maxMove = _moved ? 1 : 2;
+
+	// TO DO
+	//this->_moved = true;
+
+	if ((myPos || dest) <= maxMove)
+	{
+		// if can move
+		for (int i = 0; i < maxMove; i++)
+		{
+			// if have Solider in the way
+		}
+	}
+	
+	// check if have kill enemy 
+
+	return false;
+}
