ae39739429d935cb57d268c06b8439cacc79a677
Lior Yehezkely <liorye03@gmail.com>
2019-12-25 16:27:11 +0200

    made the king class together with actor, a canmove method and an is alerted method that checks whetehr or not the king is alerted

---
+++ King.cpp
@@ -0,0 +1,22 @@
+#include "King.h"
+
+King::King(const Position& position, const Color& color) :
+	Solider(position, color)
+{
+
+}
+bool King::canMove(const Position& dest, const Board& gameBoard) const
+{
+	return (abs((int)this->position().column() - (int)dest.column()) == 1 || abs((int)this->position().row() - (int)dest.row()) == 1);
+}
+bool King::isAlerted(const Board& gameBoard)
+{
+	for (auto solider : gameBoard)
+	{
+		if (solider != nullptr && solider->color() != this->color() && solider->canMove(this->position()))
+		{
+			return true;
+		}
+	}
+	return false;
+}
--- King.h
+++ King.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Solider.h"
+#include <cmath>
 class King : public Solider
 {
 public:
-	King(Position position, Color color);
+	King(const Position& position, const Color& color);
 	virtual bool canMove(const Position& dest, const Board& gameBoard) const override;
+	bool isAlerted(const Board& gameBoard);
 };
 
