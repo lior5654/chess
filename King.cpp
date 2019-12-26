8813f5a9ee71f12de7def9a4449e2f91be4881e5
Lior Yehezkely <liorye03@gmail.com>
2019-12-26 12:32:19 +0200

    altered the King desgin

--- King.cpp
+++ King.cpp
@@ -7,13 +7,14 @@ King::King(const Position& position, const Color& color) :
 }
 bool King::canMove(const Position& dest, const Board& gameBoard) const
 {
-	return (abs((int)this->position().column() - (int)dest.column()) == 1 || abs((int)this->position().row() - (int)dest.row()) == 1);
+	return (this->position() - dest) == 1 || (this->position() || dest) == 1;
 }
 bool King::isAlerted(const Board& gameBoard)
 {
-	for (auto solider : gameBoard)
+	unsigned i = 0;
+	for (i = 0; i < BOARD_SIZE * BOARD_SIZE; i++)
 	{
-		if (solider != nullptr && solider->color() != this->color() && solider->canMove(this->position()))
+		if (gameBoard[i] != nullptr && gameBoard[i]->color() != this->color() && gameBoard[i]->canMove(this->position(), gameBoard))
 		{
 			return true;
 		}
