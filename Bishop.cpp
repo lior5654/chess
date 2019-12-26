b46009cb925c01c1d97e9c91fb89116e5e852803
Jacob Galam <hgecdkto@gmail.com>
2019-12-26 09:52:26 +0200

    add Bishop.cpp file

--- Bishop.cpp
+++ Bishop.cpp
@@ -1,2 +1,16 @@
 #include "Bishop.h"
 
+Bishop::Bishop()
+{
+
+}
+
+Bishop::Bishop(const Position& position, const Color& color) : Solider(position, color)
+{
+
+}
+
+bool Bishop::canMove(const Position& dest, const Board& gameBoard)
+{
+	return false;
+}
